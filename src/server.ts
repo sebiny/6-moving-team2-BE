275eaa20e9211594e35976bc70f50552061a2bfc
LEE JI SU <afafmm1903@gmail.com>
2025-07-03 17:47:48 +0900

    chore:초기 셋팅 변경

--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,5 @@ import 'dotenv/config';
 const port = process.env.PORT ?? 4000;
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port! ${port}`);
 });
