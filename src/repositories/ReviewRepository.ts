82cde698233b952021e1e2e28140dd889d90b543
sebiny <rucy8888@naver.com>
2025-07-10 18:33:28 +0900

    feat: 작성가능한 리뷰 불러오기GET

--- src/repositories/ReviewRepository.ts
+++ src/repositories/ReviewRepository.ts
@@ -1,4 +1,34 @@
 import prisma from '../config/prisma';
 import type { Review, EstimateRequest, Prisma } from '@prisma/client';
 
-export async function findById(id: String) {}
+export async function findAllCompleted() {
+  return await prisma.estimateRequest.findMany({
+    where: {
+      status: 'COMPLETED'
+    },
+    select: {
+      id: true,
+      moveType: true,
+      moveDate: true,
+      fromAddress: true,
+      toAddress: true,
+      estimates: {
+        where: {
+          status: 'ACCEPTED'
+        },
+        select: {
+          price: true,
+          driver: {
+            select: {
+              profileImage: true,
+              shortIntro: true
+            }
+          }
+        }
+      }
+    }
+  });
+  // EstimateRequest의 status가 COMLPETED면은 가져오기
+  //moveType, fromAddressId, toAddressId, moveDate
+  //estimates의 price, driver의 profileImage, shortIntro
+}
