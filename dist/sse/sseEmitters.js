60f8e6c9549f21a139a3403bb2e82af0f7d28d2f
Sue <sue@Sueui-MacBookPro.local>
2025-07-17 14:28:47 +0900

    fix:remove dist folder

--- dist/sse/sseEmitters.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sseEmitters = void 0;
-exports.sseEmitters = {};
-//# sourceMappingURL=sseEmitters.js.map
--- dist/types/userType.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserType = void 0;
-exports.UserType = {
-    CUSTOMER: 'CUSTOMER',
-    DRIVER: 'DRIVER'
-};
-//# sourceMappingURL=userType.js.map
